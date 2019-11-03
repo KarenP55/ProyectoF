0c8c3f19ce0f502254a9ff9b6dc34658e8c2174a
lauvasquez <laluvll@gmail.com>
2019-11-02 20:28:08 -0400

    Avance app movil

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBHZtTESaVvWxmQIRsi46Vl_CBSaF-DK5k",
+    authDomain: "login-e3b2f.firebaseapp.com",
+    databaseURL: "https://login-e3b2f.firebaseio.com",
+    projectId: "login-e3b2f",
+    storageBucket: "login-e3b2f.appspot.com",
+    messagingSenderId: "900012192561",
+    appId: "1:900012192561:web:31c466d70cfd030ca1b610"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
