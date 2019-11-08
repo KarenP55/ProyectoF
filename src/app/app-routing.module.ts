568d8b912f0ef65e23f772d777fbce00e6eb4140
RUBÉN MALDONADO <romancalli@hotmail.com>
2019-11-08 15:55:35 -0400

    Desarrollo del mapa/incompleto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canActivate: [AuthGuard]
+},
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'admin', loadChildren: './admin/admin.module#AdminPageModule',
+  canActivate: [AuthGuard]
+},
+  
+  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
+  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' },
+  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'trabajosdetalles', loadChildren: './trabajosdetalles/trabajosdetalles.module#TrabajosdetallesPageModule' },
+  { path: 'almacen', loadChildren: './almacen/almacen.module#AlmacenPageModule' },
+  { path: 'ayuda', loadChildren: './ayuda/ayuda.module#AyudaPageModule' },
+  { path: 'clientes', loadChildren: './clientes/clientes.module#ClientesPageModule' },
+  { path: 'map', loadChildren: './tab2/map.html' },  
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/trabajosdetalles/trabajosdetalles.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trabajosdetalles',
+  templateUrl: './trabajosdetalles.page.html',
+  styleUrls: ['./trabajosdetalles.page.scss'],
+})
+export class TrabajosdetallesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
